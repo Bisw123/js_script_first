4cd97493034380c30bc4c298ad758bbe3166b3b9
Biswanath Bhuyan <100110265+Bisw123@users.noreply.github.com>
2023-10-25 05:49:33 +0000

    conversion of datatypes

---
+++ 01_BASIC/03.conversionOperation.js
@@ -0,0 +1,27 @@
+let score = true
+
+console.log(typeof score );
+console.log(typeof(score));
+
+let valueInNumber = Number(score)
+console.log(typeof valueInNumber);
+console.log(valueInNumber);
+
+//"33"=>33
+//"33abc" => NaN  (Not an Number)
+//true => 1 , false => 0
+
+let isLoggedIn="biswanth"
+let booleanIsLoggedIn = Boolean(isLoggedIn)
+console.log(booleanIsLoggedIn);
+
+//1=>true ,0=>false 
+//" " =>false 
+//"biswanath" => true 
+
+let someNumber= 33
+
+let stringNumber = String(someNumber)
+console.log(stringNumber);
+console.log(typeof stringNumber);
+
