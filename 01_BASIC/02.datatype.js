8e01672c670f3ec30479c872c11449fe7f9a6155
Biswanath Bhuyan <100110265+Bisw123@users.noreply.github.com>
2023-10-25 05:11:36 +0000

    datatypes

---
+++ 01_BASIC/02.datatype.js
@@ -0,0 +1,21 @@
+"use strict"; //treat all js code as newer version
+
+//alert("hello")// we are useing nodejs, not browser 
+
+console.log(3+3)
+console.log("Biswanath") //code readability should be high
+
+let name = "Biswanath" //string
+let age =21  //value
+let isLoggedIn = true //boolean
+
+//number => 2 to power 53
+//bigint (use for big number)
+//string => " "
+//boolean => true/false 
+//null =>standalone value (null is object )
+//undefined => value is not assigned (undefined type is undefined )
+//symbol => unique 
+
+//object 
+console.log(typeof"Biswanath ");
